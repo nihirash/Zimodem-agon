5b83f3bf692dfdb05c741b3354f30ea8b0aeefa0
Alexander Sharihin <anihirash@gmail.com>
2023-06-09 20:42:15 +0400

    Default speed is 115200

--- zimodem/zimodem.ino
+++ zimodem/zimodem.ino
@@ -49,7 +49,7 @@ const char compile_date[] = __DATE__ " " __TIME__;
 #   define DEFAULT_PIN_HS 15
 #   define DEFAULT_PIN_WIFI 0
 # endif
-# define DEFAULT_HS_BAUD 38400
+# define DEFAULT_HS_BAUD 115200
 # define DEFAULT_AA_ACTIVE LOW
 # define DEFAULT_AA_INACTIVE HIGH
 # define DEFAULT_HS_ACTIVE LOW
@@ -133,7 +133,7 @@ const char compile_date[] = __DATE__ " " __TIME__;
 # define DEFAULT_DTR_LOW  HIGH
 #endif
 
-#define DEFAULT_BAUD_RATE 1200
+#define DEFAULT_BAUD_RATE 115200
 #define DEFAULT_SERIAL_CONFIG SERIAL_8N1
 #define MAX_PIN_NO 50
 #define INTERNAL_FLOW_CONTROL_DIV 380
